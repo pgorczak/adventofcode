5d56739882f186584ffd614e40032014ab57284a
Philipp Gorczak <p.gorczak@gmail.com>
2017-12-05 15:33:07 +0100

    refactored 2017-5 (shamelessly stolen from @mfikes)

--- src/aoc2017/day_5.clj
+++ src/aoc2017/day_5.clj
@@ -1,17 +1,21 @@
 (ns aoc2017.day-5)
 
-(def input (slurp (clojure.java.io/resource "2017-5")))
-(def offsets (->> input clojure.string/split-lines (map #(Integer/parseInt %))))
+(def offsets (->> (clojure.java.io/resource "2017-5")
+                  clojure.java.io/reader
+                  line-seq
+                  (map #(Integer/parseInt %))
+                  (into [])))
 
-(defn exit-maze [seq update-fn]
-  (let [offsets (into [] seq)
-        n-offsets (count offsets)
-        outside? (fn [idx] (or (neg? idx) (>= idx n-offsets)))]
-    (loop [off offsets idx 0 ctr 0]
-      (if (outside? idx)
-        ctr
-        (recur (update off idx update-fn) (+ idx (get off idx)) (inc ctr))))))
+(defn exit-maze [offsets update-fn]
+  (loop [offsets offsets
+         index 0
+         [count & counter] (range)]
+    (if-let [jump (get offsets index)]
+      (recur (update offsets index update-fn)
+             (+ index jump)
+             counter)
+      count)))
 
 (defn solve []
-  {:part1 (exit-maze offsets inc)
-   :part2 (exit-maze offsets (fn [o] (if (>= o 3) (dec o) (inc o))))})
+  {:part-1 (exit-maze offsets inc)
+   :part-2 (exit-maze offsets (fn [o] (if (>= o 3) (dec o) (inc o))))})
