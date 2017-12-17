903426f14c817360098c7d5d8c03934cb40ccd1d
Philipp Gorczak <p.gorczak@gmail.com>
2017-12-17 13:17:07 +0100

    2017-11

---
+++ src/aoc2017/day_11.clj
@@ -0,0 +1,21 @@
+(ns aoc2017.day-11
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn abs [x] (if (neg? x) (- x) x))
+
+(def hex-steps {:n [0 2] :nw [-1 1] :sw [-1 -1] :s [0 -2] :se [1 -1] :ne [1 1]})
+
+(defn hex-step [[x1 y1] dir]
+  (let [[x2 y2] (dir hex-steps)]
+    [(+ x1 x2) (+ y1 y2)]))
+
+(defn hex-distance [xy]
+  (->> (map abs xy) (apply +) (* (/ 1 2))))
+
+(defn solve []
+  (let [input (-> "2017-11" io/resource slurp str/trim (str/split #",")
+                  (as-> strings (map keyword strings)))
+        path (reductions hex-step [0 0] input)]
+    {:part-1 (-> path last hex-distance)
+     :part-2 (->> path (map hex-distance) (apply max))}))
