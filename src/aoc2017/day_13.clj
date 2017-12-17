88473c16f19a5c4aff26a73a5ea9cdb68aa14fbd
Philipp Gorczak <p.gorczak@gmail.com>
2017-12-17 21:58:45 +0100

    refactor 2017-13 - be lazy and stop early (also range 1 doesn't exist)

--- src/aoc2017/day_13.clj
+++ src/aoc2017/day_13.clj
@@ -6,26 +6,19 @@
   (let [[_ layer depth] (re-find #"(\d+):\s(\d+)" s)]
     [(Integer/parseInt layer) (Integer/parseInt depth)]))
 
-(defn interval [depth]
-  (if (= 1 depth)
-    1
-    (* 2 (dec depth))))
-
 (defn severity [range depth]
-  (if (->> (interval depth) (mod range) zero?)
+  (if (->> (* 2 (dec depth)) (mod range) zero?)
     (* range depth)
     0))
 
-(defn path-severity [delay firewalls]
-  (->> firewalls
-       (map (fn [[range depth]] (severity (+ range delay) depth)))
-       (apply +)))
+(defn severities [delay firewalls]
+  (map (fn [[range depth]] (severity (+ range delay) depth)) firewalls))
 
 (defn solve []
   (let [input (->> "2017-13" io/resource io/reader line-seq
                    (map parse-line))]
-    {:part-1 (path-severity 0 input)
+    {:part-1 (->> (severities 0 input) (apply +))
      :part-2 (->> (range)
-                  (map (fn [x] [x (path-severity x input)]))
-                  (drop-while #(-> % second pos?))
+                  (map (fn [x] [x (severities x input)]))
+                  (filter #(->> % second (every? zero?)))
                   ffirst)}))
