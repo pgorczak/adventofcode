3047fc1534ef95045eb96f1be0a10514671b8b27
Philipp Gorczak <p.gorczak@gmail.com>
2017-12-17 13:22:02 +0100

    refactor 2017-11 - don't invent your own abs

--- src/aoc2017/day_11.clj
+++ src/aoc2017/day_11.clj
@@ -2,16 +2,14 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn abs [x] (if (neg? x) (- x) x))
-
 (def hex-steps {:n [0 2] :nw [-1 1] :sw [-1 -1] :s [0 -2] :se [1 -1] :ne [1 1]})
 
 (defn hex-step [[x1 y1] dir]
   (let [[x2 y2] (dir hex-steps)]
     [(+ x1 x2) (+ y1 y2)]))
 
-(defn hex-distance [xy]
-  (->> (map abs xy) (apply +) (* (/ 1 2))))
+(defn hex-distance [[x y]]
+  (-> (+ (Math/abs x) (Math/abs y)) (/ 2)))
 
 (defn solve []
   (let [input (-> "2017-11" io/resource slurp str/trim (str/split #",")
