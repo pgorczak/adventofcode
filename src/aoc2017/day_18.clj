9820f041b7f75d0ea47f46524c7edb80cf6cde17
Philipp Gorczak <p.gorczak@gmail.com>
2017-12-22 18:54:52 +0100

    refactor 2017-18 - happy with this!

--- src/aoc2017/day_18.clj
+++ src/aoc2017/day_18.clj
@@ -8,28 +8,20 @@
 (defn duet-update [m k f x]
   (assoc m k (f (duet-value m k) (duet-value m x))))
 
-(def dispatch
-  {:set (fn [reg x frame]
-          (assoc frame reg (duet-value frame x)))
-   :add (fn [reg x frame]
-          (duet-update frame reg + x))
-   :mul (fn [reg x frame]
-          (duet-update frame reg * x))
-   :mod (fn [reg x frame]
-          (duet-update frame reg mod x))
-   :jgz (fn [x y frame]
-          (if (pos? (duet-value frame x))
-              (update frame :pc + (dec (duet-value frame y)))
-              frame))})
+(def basic-frame
+  {:pc 0
+   :fn/set (fn [f x y] (assoc f x (duet-value f y)))
+   :fn/add (fn [f x y] (duet-update f x + y))
+   :fn/mul (fn [f x y] (duet-update f x * y))
+   :fn/mod (fn [f x y] (duet-update f x mod y))
+   :fn/jgz (fn [f x y]
+             (if (pos? (duet-value f x))
+                 (update f :pc + (dec (duet-value f y)))
+                 f))})
 
-(defn dynamic [inst]
-  (fn [x y frame]
-    ((inst frame) frame x y)))
-
-(defn execute [instrs {pc :pc :as frame}]
-  (if (< -1 pc (count instrs))
-    (-> ((get instrs pc) frame)
-        (update :pc inc))
+(defn execute [program {pc :pc :as frame}]
+  (if-let [[inst args] (get program pc nil)]
+    (-> (frame inst) (apply frame args) (update :pc inc))
     frame))
 
 (defn communicate [from-frame to-frame]
@@ -51,30 +43,31 @@
     (re-matches #"[a-z]" v) v
     :else (Integer/parseInt v)))
 
-(defn parse-line [dispatch l]
-  (let [[i x y] (str/split (str/trim l) #" ")]
-    (-> (get dispatch (keyword i) (dynamic (keyword i)))
-        (partial (parse-value x) (parse-value y)))))
+(defn parse-line [l]
+  (let [[i arg1 arg2] (str/split (str/trim l) #" ")]
+    [(keyword (str "fn/" i)) (map parse-value [arg1 arg2])]))
 
 (def p1-init-frame
-  {:pc 0
-   :snd (fn [f r _]
-          (assoc f :snd/value (duet-value f r)))
-   :rcv (fn [f r _]
-          (if (zero? (duet-value f r)) f (assoc f :rcv/value (:snd/value f))))})
+  (assoc
+   basic-frame
+   :fn/snd (fn [f r _]
+             (assoc f :snd (duet-value f r)))
+   :fn/rcv (fn [f r _]
+             (if (zero? (duet-value f r)) f (assoc f :rcv (f :snd))))))
 
 (def p2-init-frame
-  {:pc 0 :sent 0 :inbox [] :outbox nil
-   :snd (fn [f r _]
-          (-> (assoc f :outbox (duet-value f r)) (update :sent inc)))
-   :rcv (fn [f r _]
-          (if-let [in (first (:inbox f))]
-            (-> (assoc f r in) (update :inbox #(subvec % 1)))
-            (update f :pc dec)))})
+  (assoc
+   basic-frame :sent 0 :inbox [] :outbox nil
+   :fn/snd (fn [f r _]
+             (-> (assoc f :outbox (duet-value f r)) (update :sent inc)))
+   :fn/rcv (fn [f r _]
+             (if-let [in (first (:inbox f))]
+               (-> (assoc f r in) (update :inbox #(subvec % 1)))
+               (update f :pc dec)))))
 
 (defn part-2 [program]
-  (loop [f0 (assoc p2-init-frame "p" 0 :id 0)
-         f1 (assoc p2-init-frame "p" 1 :id 1)]
+  (loop [f0 (assoc p2-init-frame "p" 0)
+         f1 (assoc p2-init-frame "p" 1)]
     (let [[f0+ f1+] (-> (map (partial execute program) [f0 f1]) exchange)]
       (if (every? true? (map stalled? [f0 f1] [f0+ f1+]))
           (:sent f1+)
@@ -82,8 +75,8 @@
 
 (defn solve []
   (let [input (->> "2017-18" io/resource io/reader line-seq)
-        program (->> input (map (partial parse-line dispatch)) (into []))]
+        program (->> input (map parse-line) (into []))]
     {:part-1 (->> (iterate (partial execute program) p1-init-frame)
-                  (drop-while #(nil? (:rcv/value %)))
-                  first :rcv/value)
+                  (drop-while #(nil? (:rcv %)))
+                  first :rcv)
      :part-2 (part-2 program)}))
