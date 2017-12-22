513e37110c767000b95598a5c959e9b500730fca
Philipp Gorczak <p.gorczak@gmail.com>
2017-12-22 22:57:08 +0100

    refactor 2017-19 - use geometry like you're supposed to

--- src/aoc2017/day_19.clj
+++ src/aoc2017/day_19.clj
@@ -2,21 +2,16 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn step [dir [row col]]
-  (case dir
-    :up [(dec row) col]
-    :down [(inc row) col]
-    :left [row (dec col)]
-    :right [row (inc col)]))
+(defn step [[row col] [y x]]
+  [(+ row y) (+ col x)])
 
-(defn turn [tubes dir pos]
-  (case dir
-    (:up :down)
-    (let [l (step :left pos)]
-      (if (contains? tubes l) [l :left] [(step :right pos) :right]))
-    (:left :right)
-    (let [u (step :up pos)]
-      (if (contains? tubes u) [u :up] [(step :down pos) :down]))))
+(defn turn [tubes pos [y x]]
+  (let [left [x (- y)]
+        right [(- x) y]
+        step-left (step pos left)]
+    (if (contains? tubes step-left)
+      [step-left left]
+      [(step pos right) right])))
 
 (defn sparsify [line]
   (keep-indexed (fn [col x] (if (not= \space x) [col x])) line))
@@ -24,14 +19,14 @@
 (defn follow [tubes pos dir]
   (if-let [x (tubes pos)]
     (lazy-seq (cons x (if (= \+ x)
-                          (let [[p d] (turn tubes dir pos)] (follow tubes p d))
-                          (follow tubes (step dir pos) dir))))))
+                          (let [[p d] (turn tubes pos dir)] (follow tubes p d))
+                          (follow tubes (step pos dir) dir))))))
 
 (defn solve []
   (let [input (->> "2017-19" io/resource io/reader line-seq (map sparsify)
                    (map-indexed (fn [r cs] (for [[c x] cs] [[r c] x])))
                    (apply concat) (into {}))
         start (->> input (filter #(-> % key first zero?)) ffirst)
-        history (follow input start :down)]
+        history (follow input start [1 0])]
     {:part-1 (->> history (filter #(Character/isLetter %)) str/join)
      :part-2 (count history)}))
