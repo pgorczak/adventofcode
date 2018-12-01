43a76a457ff140dc6676f753741229ac2bbbd9b6
Philipp Gorczak <p.gorczak@gmail.com>
2018-12-01 18:28:38 +0100

    2018-01 forgot about the beautiful reductions fn
    also (contains? set thing) -> (set thing)
    a clj-set is a map of contained elements to themselves and all others to nil

--- src/aoc2018/day_1.clj
+++ src/aoc2018/day_1.clj
@@ -2,12 +2,11 @@
   (:require [aoc-utils]))
 
 (defn first-twice [input]
-  (loop [freq 0
-         seen #{}
-         [change & rest] (cycle input)]
-    (if (contains? seen freq)
+  (loop [[freq & rest] (->> input cycle (reductions + 0))
+         seen #{}]
+    (if (seen freq)
       freq
-      (recur (+ freq change) (conj seen freq) rest))))
+      (recur rest (conj seen freq)))))
 
 (defn solve []
   (let [input (map #(Integer/parseInt %) (aoc-utils/lines "2018-1"))]
