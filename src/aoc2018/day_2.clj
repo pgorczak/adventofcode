7d8e634616993791c54120bc06fee75e16a4b004
Philipp Gorczak <p.gorczak@gmail.com>
2018-12-02 14:02:48 +0100

    2018-02 part 2 - high thread count code

--- src/aoc2018/day_2.clj
+++ src/aoc2018/day_2.clj
@@ -8,7 +8,32 @@
        frequencies
        ((fn [counts] (* (get counts 2 0) (get counts 3 0))))))
 
+(defn hamming [x y]
+  (->> (map vector x y)
+       (filter (partial apply not=))
+       count))
+
+(defn common-str [x y]
+  (->> (map vector x y)
+       (filter (partial apply =))
+       (map first)
+       (apply str)))
+
+(defn pairs [x]
+  (loop [[e & r :as s] (set x)
+         ps '()]
+    (if r
+      (recur (disj s e)
+             (->> r (map #(vec [e %])) (concat ps)))
+      ps)))
+
+(defn find-common-str [input]
+  (->> (pairs input)
+       (filter #(= 1 (apply hamming %)))
+       first
+       (apply common-str)))
+
 (defn solve []
   (let [input (aoc-utils/lines "2018-2")]
     {:part-1 (checksum input)
-     :part-2 nil}))
+     :part-2 (find-common-str input)}))
